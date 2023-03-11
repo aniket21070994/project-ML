52877d4d42d9dffea89a98a87392cbfcbd1c5329
AMP <aniket.mankar001@gmail.com>
2023-03-12 03:14:06 +0530

    Updated files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    requirement=[]
+    with open(file_path) as file_obj:
+        requirement=file_obj.readlines()
+        requirement= [req.replace("\n","")for req in requirement]
+        if "-e ." in requirement:
+            requirement.remove("-e .")
+    return requirement
+
+
+setup(name="MLproject",version="0.0.1",author="Amp",author_email="aniket.mankar001@gmail.com",packages=find_packages(),install_requires=get_requirements("requirements.txt"))
